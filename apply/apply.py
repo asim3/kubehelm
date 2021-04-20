c37d160a794c72576214aeb1e0f9be8d11a73440
asim3 <asimwebapps@gmail.com>
2021-04-21 01:56:00 +0300

    added: k8s deploy_new

--- apply/apply.py
+++ apply/apply.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+from kubernetes.config import load_kube_config
+from kubernetes.client import ApiClient, CoreApi
+from kubernetes.client.rest import ApiException
+from kubernetes.utils.create_from_yaml import create_from_dict, FailToCreateError
+
+import json
+import yaml
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,8 +25,58 @@ class Apply:
         if len(self.command) < 1 or not hasattr(self, self.command[0]):
             self.help()
         else:
+            load_kube_config()
             handler = getattr(self, self.command[0])
             handler()
 
-    def setup(self):
-        print("setup:", self.command)
+    def get_namespace(self, name):
+        return name
+
+    def get_template(self, path):
+        template_path = "templates/%s.yaml" % path
+        with open(BASE_DIR / template_path, 'r') as template:
+            return template.read()
+
+    def get_template_as_list(self, path):
+        return list(yaml.safe_load_all(self.get_template(path)))
+
+    def deploy_new(self, data):
+        k8s_client = ApiClient()
+        template_as_list = self.get_template_as_list('test')
+
+        failures = []
+        for data in template_as_list:
+            try:
+                create_from_dict(k8s_client, data, dry_run="All")
+            except FailToCreateError as failure:
+                failures.extend(failure.api_exceptions)
+
+        if failures:
+            for fail in failures:
+                body = json.loads(fail.body)
+                text = "%s[%s]: %s" % (
+                    fail.reason, fail.status, body.get('message'))
+                print(text)
+            print("=" * 80)
+            raise FailToCreateError(failures)
+        else:
+            for data in template_as_list:
+                create_from_dict(k8s_client, data)
+
+    def status(self):
+        versions = CoreApi().get_api_versions()
+        print("version:", versions.versions[0])
+        print("kind:", versions.kind)
+        print("client_cidr:",
+              versions.server_address_by_client_cid_rs[0].client_cidr)
+        print("server_address:",
+              versions.server_address_by_client_cid_rs[0].server_address)
+
+    def test(self):
+        data = {
+            "name": "my-dddd-name",
+        }
+        # data = V1Deployment()
+        # print(data.__doc__)
+
+        self.deploy_new(data)
