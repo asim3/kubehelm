446db4a7863eed341a5ca2f11097e940f6b22272
asim3 <asimwebapps@gmail.com>
2021-04-25 01:54:23 +0300

    fix: update_ingress.bash

--- scripts/update_ingress.bash
+++ scripts/update_ingress.bash
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Source: https://github.com/kubernetes/ingress-nginx/releases
+
 BASE_DIR="${1}"
 
 version='0.34.1'
 
-curl -s -o ${BASE_DIR}/templates/ingress/do_main.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v${version}/deploy/static/provider/do/deploy.yaml
 
-curl -s -o ${BASE_DIR}/templates/ingress/do.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/do/deploy.yaml
-curl -s -o ${BASE_DIR}/templates/ingress/aws.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/aws/deploy.yaml
-curl -s -o ${BASE_DIR}/templates/ingress/cloud.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud/deploy.yaml
+rm -rf /tmp/ingress-nginx-controller
+mkdir  /tmp/ingress-nginx-controller
+cd     /tmp/ingress-nginx-controller
 
-curl -s -o ${BASE_DIR}/templates/ingress/kind.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
-curl -s -o ${BASE_DIR}/templates/ingress/scw.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/scw/deploy.yaml
-
-curl -s -o ${BASE_DIR}/templates/ingress/baremetal.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/baremetal/deploy.yaml
+curl -LO https://github.com/kubernetes/ingress-nginx/archive/refs/tags/controller-v${version}.tar.gz \
+  && tar -xzf controller-v${version}.tar.gz \
+  && mv /tmp/ingress-nginx-controller/ingress-nginx-controller-v${version}/deploy/static/provider/do/deploy.yaml        ${BASE_DIR}/templates/ingress/do.yaml \
+  && mv /tmp/ingress-nginx-controller/ingress-nginx-controller-v${version}/deploy/static/provider/aws/deploy.yaml       ${BASE_DIR}/templates/ingress/aws.yaml \
+  && mv /tmp/ingress-nginx-controller/ingress-nginx-controller-v${version}/deploy/static/provider/cloud/deploy.yaml     ${BASE_DIR}/templates/ingress/cloud.yaml \
+  && mv /tmp/ingress-nginx-controller/ingress-nginx-controller-v${version}/deploy/static/provider/kind/deploy.yaml      ${BASE_DIR}/templates/ingress/kind.yaml \
+  && mv /tmp/ingress-nginx-controller/ingress-nginx-controller-v${version}/deploy/static/provider/scw/deploy.yaml       ${BASE_DIR}/templates/ingress/scw.yaml \
+  && mv /tmp/ingress-nginx-controller/ingress-nginx-controller-v${version}/deploy/static/provider/baremetal/deploy.yaml ${BASE_DIR}/templates/ingress/baremetal.yaml
