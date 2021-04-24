1843d0e330e553bbef871d513a7da659a43a660b
asim3 <asimwebapps@gmail.com>
2021-04-24 17:49:42 +0300

    update: Command __init__

--- core/command.py
+++ core/command.py
@@ -2,22 +2,24 @@ from unittest import TestLoader, TextTestRunner
 from subprocess import run, PIPE
 # from argparse import ArgumentParser
 
-from manifests.apps import Whoami
+from manifests import apps
 from conf import settings
 
 
 class Command:
     def __init__(self, *args):
-        if 1 < len(args):
-            command = getattr(self, args[1], None)
-            if command:
-                return command(*args[2:])
+        command = ""
+        for i, arg in enumerate(args[1:], 2):
+            command = arg if not command else "%s_%s" % (command, arg)
+            method = getattr(self, command, None)
+            if method:
+                return method(*args[i:])
         self._print_help()
 
     def _print_help(self):
         for method in self.__dir__():
             if not method.startswith('_'):
-                print(method)
+                print(method.replace('_', ' '))
 
     def _run_script(self, path, *args):
         script = "%s %s %s" % (path, settings.BASE_DIR, " ".join(args))
@@ -28,10 +30,15 @@ class Command:
         loader = TestLoader().discover(settings.BASE_DIR / "tests")
         TextTestRunner().run(loader)
 
-    def apply(self, *args):
+    def apply_whoami(self, *args):
         namespace = input('Enter your namespace (default): ') or "default"
         app_name = input('Enter your app name: ')
-        Whoami(namespace=namespace, app_name=app_name).apply()
+        apps.Whoami(namespace=namespace, app_name=app_name).apply()
+
+    def apply_wordpress(self, *args):
+        namespace = input('Enter your namespace (default): ') or "default"
+        app_name = input('Enter your app name: ')
+        apps.Wordpress(namespace=namespace, app_name=app_name).apply()
 
     def ingress(self, *args):
         path = settings.BASE_DIR / "scripts/update_ingress.bash"
--- manifests/apps.py
+++ manifests/apps.py
@@ -4,3 +4,8 @@ from .manifest import Manifest
 class Whoami(Manifest):
     template_name = 'whoami.yaml'
     required_context = ["namespace", "app_name"]
+
+
+class Wordpress(Manifest):
+    template_name = 'wp.yaml'
+    required_context = ["namespace", "app_name"]
