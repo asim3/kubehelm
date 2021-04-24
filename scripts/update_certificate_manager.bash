e3265c9b604a5f1d527db6a5304f54f07781253a
asim3 <asimwebapps@gmail.com>
2021-04-25 02:15:29 +0300

    added: update_certificate_manager.bash

---
+++ scripts/update_certificate_manager.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Source: https://github.com/jetstack/cert-manager/releases
+
+BASE_DIR="${1}"
+
+version='1.1.1'
+
+rm -rf /tmp/cert-manager
+mkdir  /tmp/cert-manager
+cd     /tmp/cert-manager
+
+curl -LO https://github.com/jetstack/cert-manager/releases/download/v${version}/cert-manager.yaml \
+  &&  mv /tmp/cert-manager/cert-manager.yaml ${BASE_DIR}/templates/ingress/cert-manager.yaml
