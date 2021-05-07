aa0eb83c26cca9d90165edc86d2aa6e37fa57eae
asim3 <asimwebapps@gmail.com>
2021-05-07 23:03:12 +0300

    added: sed to upgrade_templates.bash

--- scripts/upgrade_templates.bash
+++ scripts/upgrade_templates.bash
@@ -2,11 +2,27 @@
 
 BASE_DIR="$(dirname $(dirname ${BASH_SOURCE[0]}))"
 
-helm repo update
+replace_app_name='s/aappppnnaammee/{{ app_name }}/'
+replace_namespace='s/nnaammeessppaaccee/{{ namespace }}/'
+replace_managed_by='s/managed-by: Helm/managed-by: Asim/'
+
+
+# helm repo update
 
 upgrade_template() {
-  helm install mmmmmm bitnami/${1} --dry-run  \
+  helm install aappppnnaammee bitnami/${1} \
+    -n nnaammeessppaaccee \
+    --dry-run  \
+    --set mariadb.enabled=false \
+    --set persistence.size=1Gi \
+    --set service.type=ClusterIP \
+    --set ingress.enabled=true \
+    --set ingress.certManager=true \
+    ${2} \
+    | sed -e "$replace_app_name" \
+    | sed -e "$replace_namespace" \
+    | sed -e "$replace_managed_by" \
     > $BASE_DIR/k8s/templates/apps/${1}.yaml
 }
 
-upgrade_template wordpress
+upgrade_template wordpress --set mariadb.enabled=false
