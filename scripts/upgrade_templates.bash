5dbaa3feacb3e7dd7f33106bab2baeaf0d9eb03b
asim3 <asimwebapps@gmail.com>
2021-05-08 05:20:09 +0300

    added: scripts / helm

--- scripts/upgrade_templates.bash
+++ scripts/upgrade_templates.bash
@@ -5,24 +5,30 @@ BASE_DIR="$(dirname $(dirname ${BASH_SOURCE[0]}))"
 replace_app_name='s/aappppnnaammee/{{ app_name }}/'
 replace_namespace='s/nnaammeessppaaccee/{{ namespace }}/'
 replace_managed_by='s/managed-by: Helm/managed-by: Asim/'
+replace_ingress='s/asimcomasimcom/{{ app_name }}.asim.com/'
 
 
 # helm repo update
 
 upgrade_template() {
-  helm install aappppnnaammee bitnami/${1} \
+  app_name=${1}
+  shift
+  chart_flags=${@}
+  helm install aappppnnaammee bitnami/${app_name} \
     -n nnaammeessppaaccee \
     --dry-run  \
-    --set mariadb.enabled=false \
-    --set persistence.size=1Gi \
-    --set service.type=ClusterIP \
     --set ingress.enabled=true \
     --set ingress.certManager=true \
-    ${2} \
+    --set ingress.tls=true \
+    --set ingress.hostname=asimcomasimcom \
+    --set service.type=ClusterIP \
+    --set persistence.size=1Gi \
+    ${chart_flags} \
     | sed -e "$replace_app_name" \
     | sed -e "$replace_namespace" \
     | sed -e "$replace_managed_by" \
-    > $BASE_DIR/k8s/templates/apps/${1}.yaml
+    | sed -e "$replace_ingress" \
+    > $BASE_DIR/k8s/templates/apps/${app_name}.yaml
 }
 
 upgrade_template wordpress --set mariadb.enabled=false
