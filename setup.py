828476d4b27bf4b06ff17b3799936a35c0277a58
Asim <asimwebapps@gmail.com>
2021-06-21 11:05:22 +0300

    update: version test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kubehelm',
-    version='0.0.6',
+    version='0.0.7',
     license='MIT',
     # https://packaging.python.org/en/latest/single_source_version.html
     keywords='sample, setuptools, helm',
