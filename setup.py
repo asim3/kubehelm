2e4f2e1f3585fac6a02f9c6574ec0d8e079a3369
asim3 <asimwebapps@gmail.com>
2021-06-21 13:22:07 +0300

    fix: setup get_version

--- setup.py
+++ setup.py
@@ -1,34 +1,50 @@
 from setuptools import setup, find_packages
 
+import os
 
-class PyPiSettings:
-    version = '0.0.8'
-    install_requires = [
-        'requests',
-        'kubernetes == 12.0.1',
-        'Jinja2==2.11.3',
-        'mysql-connector-python', ]
-    package_data = {
-        'k8s': [
-            'scripts/*',
-            'templates/*/*/*',
-            'templates/*/*',
-            'templates/*', ], }
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
 
 
 setup(
     name='kubehelm',
-    version=PyPiSettings.version,
+    version=get_version("k8s/__init__.py"),
+    long_description=long_description,
     license='MIT',
-    # https://packaging.python.org/en/latest/single_source_version.html
-    keywords='sample, setuptools, helm',
+    keywords='kubernetes, deploy, production, helm, ingress, network',
     python_requires='>=3.6, <4',
     packages=find_packages(include=['k8s', 'k8s.*']),
-    install_requires=PyPiSettings.install_requires,
-    package_data=PyPiSettings.package_data,
+    package_data={
+        'k8s': [
+            'scripts/*',
+            'templates/*/*/*',
+            'templates/*/*',
+            'templates/*',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'kubehelm = k8s.controller:execute_from_command_line',
         ],
     },
+    install_requires=[
+        'requests',
+        'kubernetes == 12.0.1',
+        'Jinja2==2.11.3',
+        'mysql-connector-python',
+    ],
 )
